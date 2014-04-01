eb4326f97ffaab6281161d967f133c681331d935
pyemkey <py.emkey@gmail.com>
2014-04-01 19:40:40 +0200

    Configure additional gems

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-LearningPlatform::Application.config.secret_key_base = '588c340a1c4ea87bcb46d3c4d25bf195eb843c4f12571ef7a41e5c6363cf2494ea99c2bc74a5200cf47370e7f11785cee627e67029e0909e6e0792d55c7b0379'
+LearningPlatform::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
