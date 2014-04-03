befa8bc7209107fae1353c212f028c364b0a7922
pyemkey <py.emkey@gmail.com>
2014-04-03 08:48:56 +0200

    Prepare seeds for Country table (name and code)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'open-uri'
+
+open("http://openconcept.ca/sites/openconcept.ca/files/country_code_drupal_0.txt") do |countries|
+
+    countries.read.each_line do |country|
+      code, name = country.chomp.split('|')
+      Country.create!(name: name, code: code)
+    end
+end
