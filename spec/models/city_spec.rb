8fec4c2e1d7af77a3b607bf30c7dff9b94d98ed9
pyemkey <py.emkey@gmail.com>
2014-04-04 18:48:37 +0200

    Add validation tests for city

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe City do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "name" do
+
+    before(:each) do
+      @london = FactoryGirl.create(:city)
+    end
+
+    context 'is provided' do
+      it 'create new city' do
+        mediolan = City.new(name: "Mediolan")
+        expect(mediolan).to be_valid
+      end    
+    end
+
+    context 'is empty' do
+      it "is invalid without name" do
+        rome = City.new()
+        expect(rome).to have(1).errors_on(:name)
+      end
+    end
+
+    context 'have to be unique' do
+      it 'is unique' do
+        mediolan = City.new(name: "Mediolan")
+        expect(mediolan).to be_valid
+      end
+
+      it 'is not unique' do
+        new_london = City.new(name: "London")
+        expect(new_london).to have(1).errors_on(:name)
+      end
+    end
+  end
 end
