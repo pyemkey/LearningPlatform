663351607a17b6eb708f32bf8a6f7c57d9bf4db6
pyemkey <py.emkey@gmail.com>
2014-04-04 18:33:34 +0200

    Add redirection after sign up action to fill demographic fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  def after_sign_in_path_for(user)
+    user.demographic_options_empty? ? edit_user_path(user) : root_path
+  end
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
