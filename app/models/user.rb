fc8fc3411b99f5e97656a166666d3b6115d6985f
pyemkey <py.emkey@gmail.com>
2014-04-04 18:36:45 +0200

    Add validation for user. It also check if any of associated objects are unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,14 +20,64 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  GENDER_TYPE = %w(male female)
 
-  belongs_to :city
-  belongs_to :state
+  belongs_to :city, autosave: true
+  belongs_to :state, autosave: true
   belongs_to :country
+  accepts_nested_attributes_for :city
+  accepts_nested_attributes_for :state
+  accepts_nested_attributes_for :country
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  GENDER_TYPE = %w(male female)
-  validates :gender, inclusion: { in: GENDER_TYPE }
+  validates :city, presence: true, on: :update
+  validates :state, presence: true, on: :update
+  validates :country, presence: true, on: :update
+  validates :gender, inclusion: { in: GENDER_TYPE }, presence: true, on: :update
+
+  def autosave_associated_records_for_city
+    begin
+      new_city = City.find_by_name(self.city.name)
+      if new_city
+        self.city = new_city
+      else
+        self.city.save!
+      end
+    rescue 
+      new_city = nil
+    end
+    
+  end
+
+  def autosave_associated_records_for_state
+    begin 
+      new_state = State.find_by_name(state.name)
+      if new_state
+        self.state = new_state
+      else
+        self.state.save!
+      end
+    rescue
+      new_state = nil
+    end
+  end
+
+  def autosave_associated_records_for_country
+    begin 
+      new_country = Country.find_by_name(country.name)
+      if new_country
+        self.country = new_country
+      else
+        self.country.save!
+      end
+    rescue
+      new_country = nil
+    end
+  end
+
+  def demographic_options_empty?
+    !(city || state || country)
+  end
 end
