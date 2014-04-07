ddc3e3f5fae09c987d97d4b5775b40001538c3d2
pyemkey <py.emkey@gmail.com>
2014-04-07 22:44:57 +0200

    Add validation tests for course

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -14,5 +14,35 @@
 require 'spec_helper'
 
 describe Course do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "presence" do
+    it "does not allow save course without title" do
+      expect(build(:course, title: nil)).to have(1).errors_on(:title)
+    end
+
+    it "does not allow save course without description" do 
+      expect(build(:course, description: nil)).to have(1).errors_on(:description)
+    end
+  end
+
+  context 'have one author' do
+    it "is invalid without author" do 
+      expect(build(:course, author_id: nil)).to have(1).errors_on(:author_id)
+    end
+    it "is valid with author" do
+      expect(build :course).to be_valid
+    end
+  end
+
+  context 'students' do
+    before(:each) do puts "Total Users:#{User.count}"
+    @course = create(:course) end
+    it "is one student" do
+      user = create(:user)
+      user.courses << @course
+      expect(@course.learners.length).to eq(1)
+    end
+    it "is two students"
+    it "none"
+  end
 end
