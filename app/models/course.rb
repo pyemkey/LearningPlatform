740bd5e95f3b71521032ae74ca3b918735a69e98
pyemkey <py.emkey@gmail.com>
2014-04-07 11:07:04 +0200

    Create association for two cases: Course has many students and has_one author

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,13 +2,18 @@
 #
 # Table name: courses
 #
-#  id          :integer          not null, primary key
-#  title       :string(255)
-#  description :text
-#  created_at  :datetime
-#  updated_at  :datetime
+#  id           :integer          not null, primary key
+#  title        :string(255)
+#  description  :text
+#  created_at   :datetime
+#  updated_at   :datetime
+#  author_id    :integer
+#  editor_id_id :integer
 #
 
 class Course < ActiveRecord::Base
-  has_many :courses
+  has_many :lessons
+  belongs_to :author, class_name: "User"
+  has_many :course_learners
+  has_many :learners, through: :course_learners
 end
