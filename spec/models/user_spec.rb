8774b73438b7742a2a436f736169da243a3c40a5
pyemkey <py.emkey@gmail.com>
2014-04-07 11:04:19 +0200

    Add tests to check association between user and course

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,18 +15,22 @@
 #  last_sign_in_ip        :string(255)
 #  created_at             :datetime
 #  updated_at             :datetime
+#  gender                 :string(255)
+#  city_id                :integer
+#  state_id               :integer
+#  country_id             :integer
+#  name                   :string(255)
 #
 
 require 'spec_helper'
 
 describe User do
-  
-  describe 'demographic fields' do
-    before(:each) do
-      @michal = FactoryGirl.create(:user)
-                  
-      end
+  before(:each) do
+    @michal = create(:user)               
+  end
    
+  describe 'demographic fields' do
+    
 
     context 'demographic fields are correct' do
       it 'confirm that demographic fields are filled in properly' do
@@ -55,6 +59,32 @@ describe User do
       it 'confirm that Country is empty' do
         @michal.country = nil
         expect(@michal.country).to be_nil
+      end
+    end
+  end
+
+  describe "have courses" do
+    context 'is an author' do
+      it "of one course" do
+        @michal.authored_courses << create(:course)
+        expect(@michal.authored_courses.count).to eq(1)
+      end
+
+      it "of two courses" do
+        2.times { @michal.authored_courses << create(:course) }
+        
+        expect(@michal.authored_courses.count).to eq(2)
+      end 
+    end
+
+    context 'is a participant' do
+      it "of one course" do
+        @michal.courses << create(:course)
+        expect( @michal.courses.count).to eq(1)
+      end
+      it "of two courses" do 
+        2.times { @michal.courses << create(:course) }
+        expect( @michal.courses.count).to eq(2)
       end
     end
   end
