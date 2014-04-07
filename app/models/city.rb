598c134a507de7234342a71a7c51783a131e0082
pyemkey <py.emkey@gmail.com>
2014-04-07 11:15:23 +0200

    Capitalize name of City before save

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,7 +1,23 @@
+# == Schema Information
+#
+# Table name: cities
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  state_id   :integer
+#  country_id :integer
+#
+
 class City < ActiveRecord::Base
   has_many :users
   belongs_to :state
   belongs_to :country
 
   validates :name, presence: true, uniqueness: true
+
+  def name=(new_name)
+    self[:name] = new_name.try(:capitalize)
+  end
 end
