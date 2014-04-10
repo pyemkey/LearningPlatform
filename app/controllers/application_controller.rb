54e140dbc39e5a37a2185db090c49e140b55e5aa
pyemkey <py.emkey@gmail.com>
2014-04-10 09:06:02 +0200

    Move sanitize attributes to devise controller and remove checker for demographic fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  def after_sign_in_path_for(user)
-    user.demographic_options_empty? ? edit_user_path(user) : root_path
-  end
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email) }
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :city, :state, :country, :gender) }
-  end
+  #, if: :devise_controller?
+  # def after_sign_in_path_for(user)
+  #   user.demographic_options_empty? ? edit_user_path(user) : root_path
+  # end
+  
 end
