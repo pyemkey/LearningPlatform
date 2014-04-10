1938e73eb6403032503558e38cd2f8a1d7791755
pyemkey <py.emkey@gmail.com>
2014-04-10 19:50:41 +0200

    Test relation between User(author, learner) and course

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -14,7 +14,7 @@
 require 'spec_helper'
 
 describe Course do
-
+  before(:each) { @course = create(:course)}
   context "presence" do
     it "does not allow save course without title" do
       expect(build(:course, title: nil)).to have(1).errors_on(:title)
@@ -27,7 +27,7 @@ describe Course do
 
   context 'have one author' do
     it "is invalid without author" do 
-      expect(build(:course, author_id: nil)).to have(1).errors_on(:author_id)
+      expect(build(:course, author_id: nil)).to have(1).errors_on(:author)
     end
     it "is valid with author" do
       expect(build :course).to be_valid
@@ -36,13 +36,22 @@ describe Course do
 
   context 'students' do
     before(:each) do puts "Total Users:#{User.count}"
-    @course = build(:course) end
+    @course = create(:course) end
     it "is one student" do
-      user = build(:user)
+      user = create(:user)
       user.courses << @course
       expect(@course.learners.length).to eq(1)
     end
-    it "is two students"
-    it "none"
+    it "is two students" do
+      2.times do 
+        user = create(:user)
+        @course.learners << user
+      end
+      expect(@course.learners.length).to eq(2)
+    end
+
+    it "none" do
+      expect(@course.learners.length).to eq(0)
+    end
   end
 end
