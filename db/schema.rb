a8e2b1c59be1b2136e929d18da13a14817d38762
pyemkey <py.emkey@gmail.com>
2014-04-15 17:49:38 +0200

    Create join table between user and lesson

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407141040) do
+ActiveRecord::Schema.define(version: 20140415124021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,11 @@ ActiveRecord::Schema.define(version: 20140407141040) do
   end
 
   add_index "lessons", ["course_id"], name: "index_lessons_on_course_id", using: :btree
+
+  create_table "lessons_users", id: false, force: true do |t|
+    t.integer "user_id",   null: false
+    t.integer "lesson_id", null: false
+  end
 
   create_table "states", force: true do |t|
     t.string   "name"
