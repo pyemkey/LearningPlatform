a5638fb89dc3305ee8a001a729d024c7d5af7c57
pyemkey <py.emkey@gmail.com>
2014-04-15 17:58:24 +0200

    Add action completed responsible_for marking lesson as finished or not

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,4 +7,15 @@ class LessonsController < ApplicationController
     course = Course.find(params[:course_id])
     @lesson = course.lessons.find(params[:id])
   end
+
+  def completed
+    course = Course.find(params[:course_id])
+    @lesson = course.lessons.find(params[:id])
+    if @lesson.isCompleted?(current_user)
+      @lesson.mark_as_uncompleted(current_user)
+    else
+      @lesson.mark_as_completed(current_user)
+    end
+    redirect_to [course, @lesson]
+  end
 end
