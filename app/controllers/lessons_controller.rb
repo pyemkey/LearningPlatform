bf06db00e3dff742bc1196525995e1cb5aecb53c
pyemkey <py.emkey@gmail.com>
2014-04-16 18:28:45 +0200

    Establish state of lesson progress [ "Not started", "In progress", "Finished"]

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,5 @@
 class LessonsController < ApplicationController
   def index
-
   end
 
   def show
@@ -9,13 +8,14 @@ class LessonsController < ApplicationController
   end
 
   def completed
-    course = Course.find(params[:course_id])
-    @lesson = course.lessons.find(params[:id])
-    if @lesson.isCompleted?(current_user)
+    @lesson = Lesson.find(params[:id])
+    if !@lesson.started?(current_user)
+      @lesson.mark_as_started(current_user)
+    elsif @lesson.isCompleted?(current_user)
       @lesson.mark_as_uncompleted(current_user)
     else
       @lesson.mark_as_completed(current_user)
     end
-    redirect_to [course, @lesson]
+    redirect_to [@lesson.course, @lesson]
   end
 end
